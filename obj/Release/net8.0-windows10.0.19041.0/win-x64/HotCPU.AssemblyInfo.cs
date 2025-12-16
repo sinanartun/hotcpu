2b544d469ca2d116bb6d3408765ba29402df9618
sinanartun <sinanartun@gmail.com>
2025-12-16 13:31:51 +0100

    feat: Add privacy policy page and generate initial build artifacts and project metadata.

--- obj/Release/net8.0-windows10.0.19041.0/win-x64/HotCPU.AssemblyInfo.cs
+++ obj/Release/net8.0-windows10.0.19041.0/win-x64/HotCPU.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A lightweight system tray application to monitor CPU temperature")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6988d98879f97838f4eee2e9635486bf0276c1ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e34bf308df6a9fb4a52722021a6f566ae260e558")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotCPU - CPU Temperature Monitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotCPU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
